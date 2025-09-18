3be32e5aeebb99888344134fea3ca83720b9b641
salak403 <salak403@SALAK403-19>
2025-09-18 07:52:37 -0500

    correcion de proyecto

--- ut_presentacion/Nucleo/EntidadesNucleo.cs
+++ ut_presentacion/Nucleo/EntidadesNucleo.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using lib_dominio.Entidades;
+
 namespace ut_presentacion.Nucleo
 {
-    class EntidadesNucleo
+    public class EntidadesNucleo
     {
-    }
-}
+        public static Usuarios? Usuarios()
+        {
+            var entidad = new Usuarios();
+            entidad.Nombre = "Pruebas-" + DateTime.Now.ToString("yyyyMMddhhmmss");
+            entidad.Pais = "Pruebas-" + DateTime.Now.ToString("yyyyMMddhhmmss");
+
+            return entidad;
+        }
+
+    }      
