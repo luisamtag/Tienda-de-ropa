b0f24e96faf23cd689f584f15fb79e4da8a4c090
arqua <arquantos66@hotmail.com>
2025-10-14 23:03:31 -0500

    primera logica de negocio

---
+++ lib_aplicaciones/Implementaciones/CarritosAplicacion.cs
@@ -0,0 +1,78 @@
+﻿using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_dominio.Entidades;
+
+namespace lib_aplicaciones.Implementaciones
+{
+    public class CarritosAplicacion : ICarritosAplicacion
+    {
+        private IConexion? IConexion = null;
+
+        public CarritosAplicacion(IConexion iConexion)
+        {
+            this.IConexion = iConexion;
+        }
+
+        public void Configurar(string StringConexion)
+        {
+            this.IConexion!.StringConexion = StringConexion;
+        }
+
+        public Carritos? Borrar(Carritos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Operaciones
+
+            this.IConexion!.Carritos!.Remove(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public Carritos? Guardar(Carritos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            // Operaciones
+
+            this.IConexion!.Carritos!.Add(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public List<Carritos> Listar()
+        {
+            return this.IConexion!.Carritos!.Take(20).ToList();
+        }
+
+        public Carritos? Modificar(Carritos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Operaciones
+
+            var entry = this.IConexion!.Entry<Carritos>(entidad);
+            entry.State = EntityState.Modified;
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+    }
+}
+
---
+++ ut_presentacion/Repositorios/CarritosAplicacionPrueba.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_aplicaciones.Implementaciones;
+using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using ut_presentacion.Nucleo;
+
+namespace ut_presentacion.Repositorios
+{
+    [TestClass]
+    public class CarritosAplicacionPrueba
+    {
+        private readonly IConexion? iConexion;
+        private readonly CarritosAplicacion? CarritosAplicacion;
+        private Carritos? entidad;
+
+        public CarritosAplicacionPrueba()
+        {
+            iConexion = new Conexion();
+            iConexion.StringConexion = Configuracion.ObtenerValor("StringConexion");
+
+            CarritosAplicacion = new CarritosAplicacion(iConexion);
+            CarritosAplicacion.Configurar(iConexion.StringConexion!);
+        }
+
+        [TestMethod]
+        public void Ejecutar()
+        {
+            Assert.AreEqual(true, Guardar());
+            Assert.AreEqual(true, Modificar());
+            Assert.AreEqual(true, Listar());
+            Assert.AreEqual(true, Borrar());
+        }
+
+        public bool Listar()
+        {
+            var lista = CarritosAplicacion?.Listar();
+            return lista.Count > 0;
+        }
+
+        public bool Guardar()
+        {
+            this.entidad = EntidadesNucleo.Carritos()!;
+            this.entidad = CarritosAplicacion?.Guardar(this.entidad)!;
+            return this.entidad.Id > 0;
+        }
+
+        public bool Modificar()
+        {
+            this.entidad!.Cliente = 2;
+            CarritosAplicacion?.Modificar(this.entidad);
+            var reloaded = iConexion.Carritos!.Find(this.entidad.Id);
+            return reloaded!.Cliente == 2;
+        }
+
+        public bool Borrar()
+        {
+            CarritosAplicacion.Borrar(this.entidad!);
+            var gone = iConexion.Carritos!.Find(this.entidad.Id);
+            return gone == null;
+        }
+    }
+}
